1223e056f205720859746b598f0d1538eee69d57
BOOK-HG1248QB44\user <minhyeong1205@gmail.com>
2024-10-29 18:21:04 +0900

    테스트를 위한 메세지 출력.

--- MFC/ChildView.cpp
+++ MFC/ChildView.cpp
@@ -49,6 +49,12 @@ void CChildView::OnPaint()
 	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
 	
 	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	dc.TextOut(10, 10, _T("Hello, MFC!")); // (10, 10) 위치에 텍스트 출력
+
+	// DrawText는 텍스트를 특정 영역에 맞춰 출력
+	CRect rect(10, 50, 200, 100);
+	dc.DrawText(_T("Hello, MFC!"), &rect, DT_CENTER | DT_VCENTER);
+
 	
 	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
 }
